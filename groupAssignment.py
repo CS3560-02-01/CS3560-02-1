b43db8f4a799fa9aefa6a05396a810226690a631
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-03-08 15:37:56 -0800

    add,remove,enroll,drop skeleton

--- groupAssignment.py
+++ groupAssignment.py
@@ -1,2 +1,18 @@
 #test
 #test2
+
+#add class to shopping cart
+def addClass():
+    return 0
+
+#remove class from shopping cart
+def removeClass():
+    return 0
+
+#enroll class 
+def enrollClass():
+    return 0
+
+#drop class
+def dropClass():
+    return 0
