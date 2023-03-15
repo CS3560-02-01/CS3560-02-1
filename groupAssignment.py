64fccd064c24b006b6ee7d093714081b7e3cbbea
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-03-15 15:54:44 -0700

    added classes

--- groupAssignment.py
+++ groupAssignment.py
@@ -2,17 +2,42 @@
 #test2
 
 #add class to shopping cart
-def addClass():
-    return 0
+class shoppingCart:
+    def addClass():
+        return 0
 
-#remove class from shopping cart
-def removeClass():
-    return 0
+    #remove class from shopping cart
+    def removeClass():
+        return 0
 
-#enroll class 
-def enrollClass():
-    return 0
+class student:
+    def enrollClass():
+        return 0
+    def dropClass():
+        return 0
+class classCourse:
+    def courseNum():
+        return 0
+    def courseName():
+        return 0
+    def courseRequirements():
+        return 0
+class courseSection:
+    def courseID():
+        return 0
+    def capacity():
+        return 0
+    def courseTime():
+        return 0
+    def professorID():
+        return 0
+    def courseRoom():
+        return 0
+class courseEnrollment:
+    def grades():
+        return 0
+    def studentID():
+        return 0
+    def courseSectionID():
+        return 0
 
-#drop class
-def dropClass():
-    return 0
