7f0bc85a86646b0d5b8984388735d741e2a0859f
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-04-09 19:06:54 -0700

    someone fix the spacing

--- enroll.py
+++ enroll.py
@@ -5,15 +5,38 @@ loginScreen = Tk()
 loginScreen.geometry('1000x500')  
 loginScreen.title('Enrollment')
 
-usernameLabel = Label(loginScreen, text="Username: ").grid(row = 0, column = 0)
+center_label = Label(loginScreen)
+center_label.grid(row=1, column=0)
+
+imgfile = "Images/cppplogo.png"
+image = PhotoImage(file = imgfile)
+image = image.subsample(2)
+imageLabel = Label(loginScreen, image=image)
+
+imageLabel.grid(row = 0, column=0, columnspan=2, padx=10, pady=10)
+
+usernameLabel = Label(center_label, text="Username: ").grid(row = 1, column = 0,sticky="e",padx=5, pady=5)
 username = StringVar()
-usernameEntry = Entry(loginScreen, textvariable = username).grid(row = 0, column = 1)  
+usernameEntry = Entry(center_label, textvariable = username).grid(row = 1, column = 1,sticky="w",padx=5, pady=5)  
 
-passwordLabel = Label(loginScreen,text = "Password: ").grid(row = 1, column = 0)  
+passwordLabel = Label(center_label,text = "Password: ").grid(row = 2, column = 0,sticky="e",padx=5, pady=5)  
 password = StringVar()
-passwordEntry = Entry(loginScreen, textvariable = password, show = '*').grid(row = 1, column = 1)  
+passwordEntry = Entry(center_label, textvariable = password, show = '*').grid(row = 2, column = 1,sticky="w",padx=5, pady=5)  
 
 
-loginButton = Button(loginScreen, text = "Login",).grid(row = 4, column = 0)  
+loginButton = Button(center_label, text = "Login",).grid(row = 3, column = 0,columnspan=2,padx=5, pady=5)  
+
+loginScreen.update_idletasks()
+width = loginScreen.winfo_width()
+height = loginScreen.winfo_height()
+x = (loginScreen.winfo_screenwidth() // 2) - (width //2 )
+y = (loginScreen.winfo_screenheight() // 2) - (height //2 )
+
+loginScreen.eval('tk::PlaceWindow . center')
+loginScreen.grid_rowconfigure(0,weight=1)
+loginScreen.grid_columnconfigure(0, weight=1)
 
 loginScreen.mainloop()
+
+
+####
