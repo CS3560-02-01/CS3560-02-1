1b2e32bafa7ebecab66cb3b45b8bdc78493e7c5d
JimmyP8899 <80071092+JimmyP8899@users.noreply.github.com>
2023-04-18 22:37:57 -0700

    Update database.py

--- database.py
+++ database.py
@@ -41,6 +41,78 @@ def get_schedule(student_id):
     results = mycursor.fetchall()
     return results
 
+def register(first_name, last_name, email_address, major, academic_standing, password, username):
+
+
+    sql = "INSERT INTO student (FirstName, LastName, EmailAddress, Major, AcademicStanding, Password, Username) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    values = (first_name, last_name, email_address, major, academic_standing, password, username)
+
+
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+def add_class_to_shoppingcart (section_id, student_id):
+
+    sql = "INSERT INTO shoppingcart (SectionID, StudentID) VALUES (%s, %s)"
+    values = (section_id, student_id)
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+def get_shoppingcart(student_id):
+
+    sql = "SELECT section.SectionID, section.Schedule, section.RoomNumber, section.InstructionMode, section.Professor, course.CourseName, course.Description " \
+          "FROM shoppingcart " \
+          "JOIN section ON shoppingcart.SectionID = section.SectionID " \
+          "JOIN course ON section.CourseID = course.CourseID " \
+          "WHERE shoppingcart.StudentID = %s"
+    values = (student_id,)
+    mycursor.execute(sql, values)
+    results = mycursor.fetchall()
+    return results
+
+def remove_class_from_shoppingcart(section_id, student_id):
+    sql = "DELETE FROM shoppingcart WHERE SectionID = %s AND StudentID = %s"
+    values = (section_id, student_id)
+    mycursor.execute(sql, values)
+    mydb.commit()
+    sql = "DELETE FROM shoppingcart WHERE StudentID = %s"
+    values = (student_id,)
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+def enroll_in_shoppingcart_classes(studentID, grade='A+'):
+    sql = "SELECT sectionID FROM shoppingcart WHERE studentID = %s"
+    values = (studentID,)
+    mycursor.execute(sql, values)
+    sectionIDs = mycursor.fetchall()
+    courses = []
+    for section in sectionIDs:
+        sql = "SELECT courseID, sectionID FROM section WHERE sectionID = %s"
+        values = (section[0],)
+        mycursor.execute(sql, values)
+        result = mycursor.fetchone()
+        courses.append(result)
+    for course in courses:
+        sql = "SELECT * FROM enrollment WHERE courseID = %s AND sectionID = %s AND studentID = %s"
+        values = (course[0], course[1], studentID)
+        mycursor.execute(sql, values)
+        result = mycursor.fetchone()
+        if result is None:
+            sql = "INSERT INTO enrollment (courseID, sectionID, studentID, grade) VALUES (%s, %s, %s, %s)"
+            values = (course[0], course[1], studentID, grade)
+            mycursor.execute(sql, values)
+    sql = "DELETE FROM shoppingcart WHERE studentID = %s"
+    values = (studentID,)
+    mycursor.execute(sql, values)
+    mydb.commit()
+
+
+def drop_course(student_id, section_id):
+    sql = "DELETE FROM enrollment WHERE studentID = %s AND sectionID = %s"
+    values = (student_id, section_id)
+    mycursor.execute(sql, values)
+    mydb.commit()
+
 #search function calling (all parameters dont need to be declared, default NONE)
 #searchclass = search_courses(course_id = '1', major = 'Computer Science', academic_standing = "Sophomore", professor = "John Smith", instruction_mode ='In-Person')
 #print(searchclass)
@@ -49,18 +121,29 @@ def get_schedule(student_id):
 #schedule = get_schedule(1)
 #print(schedule)
 
+#register new student
+#register_output = register('test', 'test', 'test@gmail.com', 'Electrical Engineering', 'Junior', 'password','testing1234')
+
+
 #add class to cart
+#add_class_to_shoppingcart(section_id=6, student_id=1)
+
+#get shopping cart classes
+#shoppingcart = get_shoppingcart(1)
+#print(shoppingcart)
 
 #remove class from cart
+#remove_class_from_shoppingcart(2, 1)
 
 #enroll in selected classes
+#enroll_in_shoppingcart_classes(1)
 
 #drop course
-
-
+#drop_course(1, 1)
 
 
 mycursor.close()
+mydb.close()
 
 
 
