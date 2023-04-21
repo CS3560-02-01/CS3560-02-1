c52c1748526a35349f68a89326f9261c6f68494a
JimmyP8899 <80071092+JimmyP8899@users.noreply.github.com>
2023-04-21 13:31:15 -0700

    documentation

--- Website/database.py
+++ Website/database.py
@@ -2,6 +2,7 @@ import mysql.connector
 
 
 class Database():
+    #establish initial connection to mySQL database 
     def __init__(self, host="localhost", user="project_user", password="password!@#123", db="enrollmentsystem"):
         self.mydb = mysql.connector.connect(
                 host = host,
@@ -10,7 +11,7 @@ class Database():
                 database = db
             )
         self.mycursor = self.mydb.cursor()
-
+    #search courses with courseID, major, academic Standing, instruction mode, and professor (fields can be left empty)
     def search_courses(self, course_id=None, major=None, academic_standing=None, instruction_mode=None, professor=None):
         sql = "SELECT course.CourseName, course.Description, section.SectionID, section.Schedule, section.RoomNumber, section.InstructionMode, section.Professor " \
             "FROM course " \
@@ -35,7 +36,8 @@ class Database():
         self.mycursor.execute(sql, values)
         results = self.mycursor.fetchall()
         return results
-
+    
+    #returns the classes that a student is enrolled in
     def get_schedule(self, student_id):
         sql = "SELECT section.SectionID, section.Schedule, section.RoomNumber, section.InstructionMode, section.Professor " \
             "FROM section " \
@@ -45,25 +47,22 @@ class Database():
         results = self.mycursor.fetchall()
         return results
 
+    #register a new student into the system
     def register(self, first_name, last_name, email_address, major, academic_standing, password, username):
-
-
         sql = "INSERT INTO student (FirstName, LastName, EmailAddress, Major, AcademicStanding, Password, Username) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         values = (first_name, last_name, email_address, major, academic_standing, password, username)
-
-
         self.mycursor.execute(sql, values)
         self.mydb.commit()
 
+    #add a course to a user's shopping cart
     def add_class_to_shoppingcart (self, section_id, student_id):
-
         sql = "INSERT INTO shoppingcart (SectionID, StudentID) VALUES (%s, %s)"
         values = (section_id, student_id)
         self.mycursor.execute(sql, values)
         self.mydb.commit()
 
+    #show courses that student added to their shopping cart
     def get_shoppingcart(self, student_id):
-
         sql = "SELECT section.SectionID, section.Schedule, section.RoomNumber, section.InstructionMode, section.Professor, course.CourseName, course.Description " \
             "FROM shoppingcart " \
             "JOIN section ON shoppingcart.SectionID = section.SectionID " \
@@ -74,6 +73,7 @@ class Database():
         results = self.mycursor.fetchall()
         return results
 
+    #delete a course from shopping cart
     def remove_class_from_shoppingcart(self, section_id, student_id):
         sql = "DELETE FROM shoppingcart WHERE SectionID = %s AND StudentID = %s"
         values = (section_id, student_id)
@@ -84,6 +84,7 @@ class Database():
         self.mycursor.execute(sql, values)
         self.mydb.commit()
 
+    #enroll student in all couurses selected in their shopping cart (default grade is A+)
     def enroll_in_shoppingcart_classes(self, studentID, grade='A+'):
         sql = "SELECT sectionID FROM shoppingcart WHERE studentID = %s"
         values = (studentID,)
@@ -110,13 +111,15 @@ class Database():
         self.mycursor.execute(sql, values)
         self.mydb.commit()
 
-
+    #drop course that student is already enrolled in
     def drop_course(self, student_id, section_id):
         sql = "DELETE FROM enrollment WHERE studentID = %s AND sectionID = %s"
         values = (student_id, section_id)
         self.mycursor.execute(sql, values)
         self.mydb.commit()
 
+    #examples of each method being called:
+    
     #search function calling (all parameters dont need to be declared, default NONE)
     #searchclass = search_courses(course_id = '1', major = 'Computer Science', academic_standing = "Sophomore", professor = "John Smith", instruction_mode ='In-Person')
     #print(searchclass)
