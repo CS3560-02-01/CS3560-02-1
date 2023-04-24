24d356e53adae2fe414d80536e68d6d1108fdc8f
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-04-24 14:40:10 -0700

    changes

--- Website/test.py
+++ Website/test.py
@@ -1,24 +1,83 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from database import Database
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user, current_user, login_required
 
 app = Flask(__name__, template_folder='templates') #added template_folder
 
-@app.route('/')
+@app.route('/', methods =['GET', 'POST'])
 def index():
-    return render_template('index.html')
 
-@app.route('/home')
-def second_page():
-    return render_template('home.html') 
+    if request.method == 'POST':
+        # handle form submission
+        username = request.form['username']
+        password = request.form['password']
+        # do something with username and password
+        return render_template('home.html')
+    else:
+        return render_template('index.html', boolean=True)
 
-@app.route('/search')
-def third():
-    return render_template('search.html') 
 
-@app.route('/search_results')
-def fourth():
+# @app.route('/login', methods =['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         email = request.form.get('username')
+#         password = request.form.get('password')
+
+#         user = Database.query.filter_by(username = username)
+#         if user:
+#             if check_password_hash(user.password, password):
+#                 flash('Login in successfully' ,category='success')
+#                 login_user(user, remember = True)
+#                 return redirect(url_for('views.home'))
+#             else: 
+#                 flash('Incorrect password, try again.', category='error')
+#         else:
+#             flash('Email does not exist.', category='error')
+#     return render_template("index.html", boolean = True)
+# @app.route('/logout', methods =['GET', 'POST'])
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for('app.login'))
+
+            
+
+
+
+# @app.route('/sign-up', methods =['GET', 'POST'])
+# def sign_up():
+#     if request.method == 'POST':
+#         firstName =request.form.get('firstName')
+#         lastName =request.form.get('lastName')
+#         email =request.form.get('email')
+#         password1 =request.form.get('password')
+#         password2 =request.form.get('password2')
+#         if len(email) < 4:
+#             pass
+#         elif len(firstName)< 2:
+#             pass
+#         elif password1 != password2:
+#             pass
+#         elif len(password1)<7:
+#             pass
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+     if request.method == 'POST':
+        return render_template('home.html') 
+    
+
+@app.route('/search', methods =['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        return render_template('search.html') 
+
+@app.route('/search_results', methods =['GET', 'POST'])
+def search_results():
     return render_template('searchresults.html') 
 
-@app.route('/fifth')
+@app.route('/fifth', methods =['GET', 'POST'])
 def fifth():
     return render_template('fifth.html')
 
